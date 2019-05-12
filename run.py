d19bba16ebd680f6eb0b9c4bc53bd2a7eb51ef42
Uomar <uomarearlie7@gmail.com>
2019-05-13 01:52:11 +0300

    function to display the  credentials

--- run.py
+++ run.py
@@ -32,3 +32,24 @@ def account_verification(first_name,password):
     '''
     verified_user = Credentials.confirm_user(first_name,password)
     return verified_user
+
+def create_credentials(user_name,account_name,site_name,site_password):
+    '''
+    function to create new credentials
+    '''
+    new_credentials = Credentials(user_name,account_name,site_name,site_password)
+    return new_credentials
+
+def save_credentials(credentials):
+    '''
+    function to save credentials
+    '''
+    Credentials.save_credentials(credentials)
+
+def display_credentials(user_name,site_name):
+    '''
+    function to display the list of credentials
+    '''
+    Credentials.display_credentials(user_name,site_name)
+
+

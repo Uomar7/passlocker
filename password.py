acf43e1fe5417943ba16008107d557a41e215e3b
Uomar <uomarearlie7@gmail.com>
2019-05-12 15:25:41 +0300

    defined the class instances.

--- password.py
+++ password.py
@@ -3,3 +3,8 @@ class User:
     class to creates instances of class User.
     '''
     user_list = []
+
+    def __init__(self,first_name,last_name,password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password

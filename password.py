c13cb969d4921b1767bfd81a093aa1e1d3003ff8
Uomar <uomarearlie7@gmail.com>
2019-05-12 16:13:43 +0300

    defined the user saving method.

--- password.py
+++ password.py
@@ -4,6 +4,13 @@ class User:
     '''
     user_list = []
 
+    def save_user(self):
+        '''
+        save user method that appends objects to our user_list list.
+        '''
+        
+        User.user_list.append(self)
+
     def __init__(self,first_name,last_name,password,email):
         self.first_name = first_name
         self.last_name = last_name

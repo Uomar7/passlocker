fd050b8910975bd4936bfbe881ebaa0e2b7b7938
Uomar <uomarearlie7@gmail.com>
2019-05-13 01:24:00 +0300

    Function to create a new user

---
+++ run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.6
+from password import User, Credentials
+
+def create_account(fname,lname,password,mail):
+    '''
+    Function to create a new user
+    '''
+    new_account = User(fname,lname,password,mail)
+    return new_account
+
